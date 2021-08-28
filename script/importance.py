14b54c7db65acbd5dda86f4a70a50b18ff7b0037
joluoch <jakindaodhiambo@gmail.com>
2021-08-28 20:43:13 +0300

    add cancer detectionclassification

--- script/importance.py
+++ script/importance.py
@@ -4,6 +4,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 import numpy as np
 # Set random seed
 seed = 42
@@ -13,38 +15,38 @@ seed = 42
 ################################
 
 # Load in the data
-df = pd.read_csv("wine_quality.csv")
+#df = pd.read_csv("wine_quality.csv")
 
-#encode diagnosis
-#scale
+data = pd.read_csv('../data/data_outlier.csv')
+
+
+
+
+
 # Split into train and test sections
-y = df.pop("quality")
-X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
+target_name = "diagnosis"
+target = data[target_name]
+data = data.drop(columns=target_name)
+
+data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.30, random_state=42)
+
 
 #################################
 ########## MODELLING ############
 #################################
 
 # Fit a model on the train section
-regr = RandomForestRegressor(max_depth=2, random_state=seed)
-regr.fit(X_train, y_train)
 
-# Report training set score
-train_score = regr.score(X_train, y_train) * 100
-# Report test set score
-test_score = regr.score(X_test, y_test) * 100
+select_feature = SelectKBest(chi2, k=5)
+select_feature.fit(data_train, target_train)
 
-# Write scores to a file
-with open("metrics.txt", 'w') as outfile:
-        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
 
 
 ##########################################
 ##### PLOT FEATURE IMPORTANCE ############
 ##########################################
 # Calculate feature importance in random forest
-importances = regr.feature_importances_
+importances = select_feature.feature_importances_
 labels = df.columns
 feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
 feature_df = feature_df.sort_values(by='importance', ascending=False,)
@@ -64,24 +66,4 @@ plt.savefig("feature_importance.png",dpi=120)
 plt.close()
 
 
-##########################################
-############ PLOT RESIDUALS  #############
-##########################################
 
-y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
-y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
-
-ax = sns.scatterplot(x="true", y="pred",data=res_df)
-ax.set_aspect('equal')
-ax.set_xlabel('True wine quality',fontsize = axis_fs) 
-ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-ax.set_title('Residuals', fontsize = title_fs)
-
-# Make it pretty- square aspect ratio
-ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-plt.ylim((2.5,8.5))
-plt.xlim((2.5,8.5))
-
-plt.tight_layout()
-plt.savefig("residuals.png",dpi=120) 
